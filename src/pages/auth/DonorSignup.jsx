26662799eab706ce2b79503ef779707080c7f0dc
Safia Bulle <safia.bulle@student.moringaschool.com>
2026-06-13 20:33:09 +0300

    Finish auth implementation

--- src/pages/auth/DonorSignup.jsx
+++ src/pages/auth/DonorSignup.jsx
@@ -34,6 +34,18 @@ const DonorSignup = () => {
             className="w-full border p-3 rounded-lg"
           />
 
+          <select className="w-full border p-3 rounded-lg bg-white">
+            <option value="">Select Blood Group</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+          </select>
+
           <input
             type="password"
             placeholder="Password"
