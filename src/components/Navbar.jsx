947a18b2ddb4b3eb5c6d115462a0b0ec98cf524b
adisacodes <marian.adisa@student.moringaschool.com>
2026-06-13 11:00:19 +0300

    update sidebar to use react router link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,15 @@ const Navbar = () => {
     <nav className="bg-red-700 text-white p-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">🩸 Blood Donation System</h1>
       <div className="flex gap-4">
-        <a href="/donors" className="hover:underline">Donors</a>
-        <a href="/requests" className="hover:underline">Requests</a>
-        <a href="/login" className="hover:underline">Login</a>
+        <Link to="/donors" className="hover:underline">
+          Donors
+        </Link>
+        <Link to="/requests" className="hover:underline">
+          Requests
+        </Link>
+        <Link to="/login" className="hover:underline">
+          Login
+        </Link>
       </div>
     </nav>
   )
