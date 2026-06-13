efcdf17028912e7c9a94abbfa7a40b5ed8cb77ac
adisacodes <marian.adisa@student.moringaschool.com>
2026-06-13 21:56:12 +0300

    resolved setup test conflict

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
 import '@testing-library/jest-dom'
-=======
-import { expect } from 'vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers); 
->>>>>>> donors
